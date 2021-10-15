ec2a5eeb778bd97ccb1eef0b6026f2603ecc4d2d
lennonsousa <lennonsousa@outlook.com>
2021-10-15 11:00:17 -0300

    Smtp secure.

--- src/modules/mailer.ts
+++ src/modules/mailer.ts
@@ -23,6 +23,7 @@ class Mailer {
         const transporter = nodemailer.createTransport({
             host: process.env.EMAIL_HOST,
             port: Number(process.env.EMAIL_PORT),
+            secure: true, // use TLS
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
