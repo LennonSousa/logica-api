7aa3bd03988a464ec6d064d71e13dd549923dc69
lennonsousa <lennonsousa@outlook.com>
2021-10-15 11:11:57 -0300

    E-mail require tls.

--- src/modules/mailer.ts
+++ src/modules/mailer.ts
@@ -24,6 +24,7 @@ class Mailer {
             host: process.env.EMAIL_HOST,
             port: Number(process.env.EMAIL_PORT),
             secure: true, // use TLS
+            requireTLS: true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
